30450570e76d4a43085071260e2c22dcd62d98dc
baconjulie <julieebacon@gmail.com>
2020-02-01 07:09:36 -0500

    disallow dem robots

--- pages/events.js
+++ pages/events.js
@@ -13,6 +13,29 @@ const Events = () => (
 
     <div className="hero">
       <h1 className="title">Events</h1>
+      <ul>
+        <li>Time, location: Ceremony</li>
+        <li>Time, location: Reception</li>
+        <li>Time, location: Sunday Brunch</li>
+      </ul>
+
+      <h1>Travel Info</h1>
+      <ul>
+        <li>Airport</li>
+        <li>Hotel address, website/number, hotel room block info</li>
+        <li>
+          Shuttle information (where it picks up/drops off, how frequently runs)
+        </li>
+      </ul>
+
+      <h1>Things to do</h1>
+      <ul>
+        <li>
+          Boston: Red Sox game at Fenway, Boston Public Gardens, Seaport
+          District, North End, Trade Boston (our first date spot)
+        </li>
+        <li>Around Danverse</li>
+      </ul>
     </div>
   </div>
 );
