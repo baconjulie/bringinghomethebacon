53721c4e8074914598a1c6a734a8cd89c84b8ddb
baconjulie <julieebacon@gmail.com>
2020-02-01 06:37:29 -0500

    add linting

--- pages/registry.js
+++ pages/registry.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
 
 const Registry = () => (
   <div>
     <Head>
       <title>Registry</title>
-      <link rel='icon' href='/favicon.ico' />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <Nav />
 
-    <div className='hero'>
-      <h1 className='title'>Registry</h1>
+    <div className="hero">
+      <h1 className="title">Registry</h1>
     </div>
   </div>
+);
 
-)
-
-export default Registry
+export default Registry;
